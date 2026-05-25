4d45d2bbdfbb11d4e56327013df91e826cadfa70
nathanbiketeixeira <nathanteixeira744@gmail.com>
2026-05-25 11:54:08 -0300

    Create agendamento.py

---
+++ src/tools/agendamento.py
@@ -0,0 +1,8 @@
+
+def agendar_teleconsulta(especialidade, urgencia):
+    return {
+        "status": "confirmado",
+        "especialidade": especialidade,
+        "urgencia": urgencia,
+        "horario": "2026-06-01 14:00"
+    }
