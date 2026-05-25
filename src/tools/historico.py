747e8be6fff89563253687cdf37b43816d4b6ae0
nathanbiketeixeira <nathanteixeira744@gmail.com>
2026-05-25 11:52:51 -0300

    Add function to consult patient history

---
+++ src/tools/historico.py
@@ -0,0 +1,9 @@
+
+def consultar_historico_paciente(paciente_id):
+    return {
+        "nome": "Maria",
+        "idade": 34,
+        "historico": ["Hipertensão"],
+        "medicamentos": ["Losartana 50mg"],
+        "ultima_consulta": "03/2026 com Dr. João"
+    }
